7d0e9f1af2167c713f514cb843722e32b67d0e55
John McCann <johnryanmccann@pm.me>
2021-12-22 07:50:45 -0600

    rfct: Rework git setup directory
    Improved the hierarchy of the git setup directory. Also made minor
    tweaks to matplotlib and vimrc configs.

--- git_custom/install_git_custom.sh
+++ git_custom/install_git_custom.sh
@@ -8,26 +8,51 @@ if test $1 -ne 0; then
 else
   git_custom_dir=~/.git_custom
   mkdir -p "$git_custom_dir" &&
-  cp "$cwd"/git_* "$git_custom_dir"
+  cp -r "$cwd"/git_* "$git_custom_dir"
 fi
-git_alias="$git_custom_dir""/git_alias"
+
+# paths
+git_alias_map="$git_custom_dir""/git_aliases/git_map"
+git_alias_init_www="$git_custom_dir""/git_aliases/git_init_www"
+git_alias_skip="$git_custom_dir""/git_aliases/git_skip"
 git_template="$git_custom_dir""/git_template"
 git_ignore="$git_custom_dir""/git_ignore"
 
 echo 'configuring git...'
 
-git config --global alias.genesis "!. "$git_alias" && git_genesis"
-git config --global alias.introduce "!. "$git_alias" && git_introduce"
-git config --global alias.intro-gitlab "!. "$git_alias" && git_introgitlab"
-git config --global alias.intro-github "!. "$git_alias" && git_introgithub"
-git config --global alias.deathOfASalesman "!. "$git_alias" && git_death_of_a_salesman"
-git config --global alias.omnistart "!. "$git_alias" && git_omnistart"
-git config --global alias.init-www "!. "$git_alias" && git_initwww"
-git config --global alias.addnw "!sh -c 'git diff -U0 -w --no-color "\$@" | git apply --cached --ignore-whitespace --unidiff-zero -'"
-git config --global alias.map "!. "$git_alias" && pretty_git_log"
-git config --global alias.mapall "!. "$git_alias" && pretty_git_log_all"
-git config --global alias.mapblame "!. "$git_alias" && pretty_git_log_blame"
-git config --global alias.mapgory "!. "$git_alias" && pretty_git_log_gory"
+# add init-www aliases
+git config --global alias.genesis \
+"!. "$git_alias_init_www" && git_genesis"
+git config --global alias.introduce \
+"!. "$git_alias_init_www" && git_introduce"
+git config --global alias.intro-gitlab \
+"!. "$git_alias_init_www" && git_introgitlab"
+git config --global alias.intro-github \
+"!. "$git_alias_init_www" && git_introgithub"
+git config --global alias.deathOfASalesman \
+"!. "$git_alias_init_www" && git_death_of_a_salesman"
+git config --global alias.omnistart \
+"!. "$git_alias_init_www" && git_omnistart"
+git config --global alias.init-www \
+"!. "$git_alias_init_www" && git_initwww"
+# add non-whitespace changes to index
+git config --global alias.addnw \
+"!sh -c 'git diff -U0 -w --no-color "\$@" | "\
+"git apply --cached --ignore-whitespace --unidiff-zero -'"
+# add map aliases
+git config --global alias.map \
+"!. "$git_alias_map" && pretty_git_log"
+git config --global alias.mapall \
+"!. "$git_alias_map" && pretty_git_log_all"
+git config --global alias.mapblame \
+"!. "$git_alias_map" && pretty_git_log_blame"
+git config --global alias.mapgory \
+"!. "$git_alias_map" && pretty_git_log_gory"
+# add skip aliases
+git config --global alias.skip \
+"!. "$git_alias_skip" && git_skip"
+git config --global alias.unskip \
+"!. "$git_alias_skip" && git_unskip"
 
 git config --global commit.template "$git_template"
 
--- matplotlib_custom/install_matplotlib_custom.sh
+++ matplotlib_custom/install_matplotlib_custom.sh
@@ -4,6 +4,10 @@ cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo 'configuring matplotlib...'
 
+if [ ! -d "~/.matplotlib/stylelib" ]; then
+  mkdir -p ~/.matplotlib/stylelib
+fi
+
 cp "$cwd"/matplotlibrc ~/.matplotlib/
 cp "$cwd"/gruvbox.mplstyle ~/.matplotlib/stylelib/
 
